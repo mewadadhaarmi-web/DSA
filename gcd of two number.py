0ba6c1f069506796a0b0f270c7909375e4bab794
mewadadhaarmi-web <mewadadhaarmi@gmail.com>
2026-03-11 11:21:50 +0530

    Add files via upload

---
+++ gcd of two number.py
@@ -0,0 +1,9 @@
+# GCD of two numbers using Euclidean Algorithm
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+while b != 0:
+    a, b = b, a % b
+
+print("GCD is:", a)
